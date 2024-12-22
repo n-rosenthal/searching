6ffffd41d3d278eacd408c620f50b4d104f0c80e
Nícolas Dal Corso <rdalcorso.nicolas@gmail.com>
2024-12-22 16:58:55 -0300

    Showing node addition operator

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Set, List;
+from src.datastructures.CommonNodes import BasicEdge, BasicGraph, BasicNode;
+from src.datastructures.primitives.NodicVisualizers import visualize, visualize_tree;
+from src.datastructures.NodicWays import Walk, Trail, Path, is_path
+from src.operators.ElementarNodicOperators import MEdge, Multigraph;
+
+def main():
+    A, B, C, D, E, F    = BasicNode("A"), BasicNode("B"), BasicNode("C"), BasicNode("D"), BasicNode("E"), BasicNode("F");
+    g                   = BasicGraph.from_nodes({A, B, C, D, E, F}, {BasicEdge(A, B), BasicEdge(B, C), BasicEdge(C, A), 
+                                                               BasicEdge(C, D), BasicEdge(D, E), BasicEdge(E, C),
+                                                               BasicEdge(E, F)});
+    
+    G, H, I, J, K, L   = BasicNode("G"), BasicNode("H"), BasicNode("I"), BasicNode("J"), BasicNode("K"), BasicNode("L");
+    h                  = BasicGraph.from_nodes({G, H, I, J, K, L}, {BasicEdge(G, H), BasicEdge(H, I), BasicEdge(I, J), 
+                                                               BasicEdge(J, K), BasicEdge(K, L), BasicEdge(L, G)});
+    
+    print(g + I);
+    
+    
+    return;
+
+if __name__ == "__main__": main();
