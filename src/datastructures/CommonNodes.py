91ac46ddecd9401af4361ce7de68ecaa34d41fa4
Nícolas Dal Corso <rdalcorso.nicolas@gmail.com>
2024-12-24 17:40:29 -0300

    Operation and properties over graphs

--- src/datastructures/CommonNodes.py
+++ src/datastructures/CommonNodes.py
@@ -9,6 +9,7 @@ Implements `Node` objects over the `INode` interface and `Edge` objects over the
 
 from src.datastructures.primitives.NodicBasis import IEdge, INode, IGraph, GraphException, NodeException
 from typing import Any, List, Tuple, Set;
+from src.operators.ElementarNodicOperators import order
 
 class BasicNode(INode):
     """
@@ -194,35 +195,113 @@ class BasicGraph(IGraph):
             
         return set(node[0] for node in nodes);
         
-        
-
-    def exist_path(self, source: INode, target: INode) -> bool:
+    @property
+    def order(self) -> int:
         """
-        Checks if a path exists between the given source and target nodes in the graph.
+        Returns the order of a `Graph` object, which is the number of nodes in the graph.
+
+        Returns:
+            int: The order of the graph.
+        """
+        return len(self.nodes);
+    
+    @property
+    def size(self) -> int:
+        """
+        Returns the size of a `Graph` object, which is the number of edges in the graph.
+
+        Returns:
+            int: The size of the graph.
+        """
+        return len(self.edges);
+    
+    @property
+    def chromatic_number(self) -> int:
+        """
+        Returns the chromatic number of a `Graph` object, which is the maximum degree of any node in the graph.
+
+        Returns:
+            int: The chromatic number of the graph.
+        """
+        return max(self.degree(node) for node in self.nodes);
+    
+    def distance(self, source: INode, target: INode) -> int:
+        """
+        Returns the distance between two nodes in the graph.
         
         Parameters
         ----------
         source : Node
-            The source node of the path.
+            The source node.
         target : Node
-            The target node of the path.
+            The target node.
         
         Returns
         -------
-        bool
-            True if a path exists, False otherwise.
+        int
+            The distance between the nodes.
         """
-        explored : Set[INode] = set();
-        frontier : List[INode] = [source];
+        return len(self.shortest_path(source, target));
+    
+    def shortest_path(self, source: INode, target: INode) -> List[INode]:
+        """
+        Returns the shortest path between two nodes in the graph.
         
-        while frontier:
-            node = frontier.pop(0);
-            if node == target:
-                return True;
-            explored.add(node);
-            for edge in self.edges:
-                if edge.source == node and edge.target not in explored:
-                    frontier.append(edge.target);
-        return False;
-
-
+        Parameters
+        ----------
+        source : Node
+            The source node.
+        target : Node
+            The target node.
+        
+        Returns
+        -------
+        List[Node]
+            The shortest path between the nodes.
+        """
+        path : List[INode] = [source];
+        while path[-1] != target:
+            path += [node for edge in self.edges if edge.source == path[-1] for node in [edge.target, edge.source] if node not in path];
+        return path;
+    
+    def get_edges(self, node: INode) -> Set[IEdge]:
+        """
+        Given a `Node` in the graph, returns the set of edges that connect to that node.
+        
+        Parameters
+        ----------
+        node : Node
+            The node whose edges are to be retrieved.
+        
+        Returns
+        -------
+        Set[Edge]
+            The set of edges that connect to the node.
+        """
+        if node in self.nodes:
+            return {edge for edge in self.edges if edge.source == node or edge.target == node};
+        else:
+            raise ValueError("The given node is not in the graph.");
+        
+    def get_nodes(self, edge: IEdge) -> Set[INode]:
+        """
+        Given an `Edge` in the graph, returns the set of nodes that are connected by that edge.
+        
+        Parameters
+        ----------
+        edge: IEdge
+            The edge whose nodes are to be retrieved.
+            
+        Returns
+        -------
+        Set[Node]
+            The set of nodes that are connected by the edge.
+        """
+        if edge in self.edges:
+            if edge.source not in self.nodes or edge.target not in self.nodes:
+                raise ValueError("The given edge is not in the graph.");
+            else:
+                return {edge.source, edge.target};
+        else:
+            raise ValueError("The given edge is not in the graph.");
+        
