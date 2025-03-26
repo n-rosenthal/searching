675b12a17ba8c1b7634a81786652cfe59e6e5426
Nícolas Dal Corso <rdalcorso.nicolas@gmail.com>
2025-03-26 17:03:59 -0300

    Neighbors function implemented

--- c-search/src/8Puzzle.cpp
+++ c-search/src/8Puzzle.cpp
@@ -12,6 +12,12 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <queue>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <sstream>
 
 /**
  * action := UP | LEFT | RIGHT | DOWN
@@ -23,22 +29,23 @@ enum Action {UP, LEFT, RIGHT, DOWN};
 std::vector<int> GOAL = {1, 2, 3, 4, 5, 6, 7, 8, 0};
 // GOAL é o estado final do jogo.
 
-
+//  Functions for analyzing the 8Puzzle game
 
 /**
- * @brief Verifica se um estado é o estado final.
- *
- * @param state Vetor de 9 elementos inteiros representando o estado.
- * @return true se state for o estado final; false caso contrário.
+ * @brief   Given an `state`, returns the position of the empty space in the 8Puzzle game as a pair of integers.
+ * 
+ * @param state `const std::array<int, 9>&`
+ *              The state of the 8Puzzle game.
+ * 
+ * @return `std::pair<int, int>`
+ *         The position of the empty space in the 8Puzzle game as a pair of integers.
  */
-bool isGoal(std::vector<int> state) {
-    for (int i = 0; i < 9; i++) {
-        if (state[i] != GOAL[i]) {
-            return false;
-        };
-    };
-    return true;
-};
+std::pair<int, int> getEmptySpacePosition(const std::array<int, 9>& state) {
+    int index = std::distance(state.cbegin(), std::find(state.cbegin(), state.cend(), 0));
+    return std::make_pair(index / 3, index % 3);
+}
+
+
 
 /**
  * @brief   An 8Puzzle node consisting of a state (vector<int>), an action (string),
@@ -57,18 +64,50 @@ struct Node {
 /**
  * @brief Creates a new node for the 8Puzzle game.
  * 
- * @param state The current state of the puzzle board, represented as a vector of integers.
- * @param action The action that led to this node, represented as a string.
- * @param parent A pointer to the parent node.
- * @return A Node structure containing the state, action, and parent information.
+ * @param state `std::array<int, 9>`
+ *              A pointer to the state of the node.
+ * @param action `Action`
+ *              The action taken to get to this node.
+ * @param parent `Node*`
+ *              The parent of this node.
+ * 
+ * @return `Node`
+ *         A new node for the 8Puzzle game.
  */
-Node getNode(array::int state, Action action, Node* parent) {
-    return {
-        .state = state,
-        .action = action,
-        .parent = parent
-    };
+Node getNode(std::array<int, 9> state, Action action, Node* parent) {
+    Node node;
+    node.state = state;
+    node.action = action;
+    node.parent = parent;
+    return node;
 };
+
+/**
+ * @brief       Given a `Node` for the 8Puzzle game, returns a vector of neighboring nodes.
+ * @details     The neighbors of a node are the nodes that can be reached by performing an action on the node.
+ *              They are always generated in the order UP, LEFT, RIGHT, DOWN.
+ * 
+ * @param       `Node` node
+ *              The `Node` for the 8Puzzle game.
+ * 
+ * @return      `std::vector<Node>`
+ *              A vector of neighboring nodes.
+ */
+std::vector<Node> getNeighbors(const Node& node) {
+    std::vector<Node> neighbors;
+    const std::array<int, 4> dx = { -3, -1, 1, 3 };
+    const std::array<Action, 4> actions = { Action::UP, Action::LEFT, Action::RIGHT, Action::DOWN };
+    int emptySpaceIndex = std::distance(node.state.cbegin(), std::find(node.state.cbegin(), node.state.cend(), 0));
+    for (int i = 0; i < 4; ++i) {
+        int newIndex = emptySpaceIndex + dx[i];
+        if (newIndex >= 0 && newIndex < 9 && (dx[i] % 3 == 0 || node.state[newIndex] != 0)) {
+            std::array<int, 9> newState = node.state;
+            std::swap(newState[emptySpaceIndex], newState[newIndex]);
+            neighbors.emplace_back(getNode(newState, actions[i], const_cast<Node*>(&node)));
+        }
+    }
+    return neighbors;
+}
 
 
 
@@ -88,13 +127,24 @@ void printState(Node node) {
 };
 
 
-
 int main() {
-    Action action = UP;
-    array::int state = {1, 2, 3, 4, 5, 6, 7, 0, 8};
+    const Action action = Action::UP;
+    std::array<int, 9> initialState = {1, 2, 3, 4, 5, 6, 7, 0, 8};
+    Node node = getNode(initialState, action, nullptr);
 
-    Node node = getNode(state, action, nullptr);
+    const auto emptySpacePosition = getEmptySpacePosition(node.state);
+    std::cout << "(" << emptySpacePosition.first << ", " << emptySpacePosition.second << ")" << std::endl;
+    
+    std::vector<Node> neighbors = getNeighbors(node);
+    for (Node neighbor : neighbors) {
+        printState(neighbor);
+        std::vector<Node> neighbors2 = getNeighbors(neighbor);
 
-    printState(node);
+        for (Node neighbor2 : neighbors2) {
+            printState(neighbor2);
+        };
+    };
+
+    return 0;
 };
 
